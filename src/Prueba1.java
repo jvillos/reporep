c8016d671fbe5e037f820c48d64d452ec11e065a
oligarc <148318306+oligarc@users.noreply.github.com>
2024-04-29 14:10:23 +0200

    Update Prueba1.java

--- src/Prueba1.java
+++ src/Prueba1.java
@@ -4,7 +4,9 @@ public class Prueba1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=10;
+		int b=57;
 		System.out.println(a);
+		System.out.println(b);
 	}
 
 }
